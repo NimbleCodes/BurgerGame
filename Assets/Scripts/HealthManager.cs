f50fd3193f0bf48cf6ca383710c8cf15d6b52be8
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2020-03-18 23:18:32 +0900

    03.18.23:00

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float fDelayTime;
+    public float fCurrentTime;
+    public int iMaxX;
+    private float fVertical;
     void Start()
     {
-        
+        iMaxX = (int)GetComponent<RectTransform>().rect.width;
+        fVertical = transform.Find("SlidingArea").Find("Handle").gameObject.GetComponent<RectTransform>().offsetMax.y;
+
+        fDelayTime = 10.0f;
+        fCurrentTime = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        fCurrentTime += Time.deltaTime;
+
+        float fT = iMaxX * (fCurrentTime / fDelayTime);
+        transform.Find("SlidingArea").Find("Handle").
+            gameObject.GetComponent<RectTransform>().offsetMax = new Vector2(fVertical + fT, 0);
     }
 }
