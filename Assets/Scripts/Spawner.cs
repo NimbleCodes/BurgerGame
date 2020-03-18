b46d7a148de3060a486604c788951f4ace0a6bc5
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2020-03-18 20:16:46 +0900

    till now
    till 03.18

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string[] Patty = new string[4] { "MeatP", "Lobster", "Chickintender", "Bulgogi" };
+    public string[] Vegy = new string[5] { "Lettus", "Onion", "Relish", "Pickle", "Potato" };
+    public string[] BreadCheese = new string[3] { "BaseB", "UpperB", "Cheese" };
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    public string[] Orders;
+    public int Patnum = Random.Range(0,4);
+    public int Vegnum = Random.Range(0,5);
+    Spawner() {
+            
+            Orders = new string[5]{ BreadCheese[0], Vegy[Vegnum], Patty[Patnum], BreadCheese[1], BreadCheese[2] };
+        }
+
+
+
 }
