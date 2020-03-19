a04df9d9c73d6d8b5fa05be095f24528eee7e5fd
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2020-03-19 18:21:08 +0900

    spawner/ tiny progress

---
+++ Assets/Scripts/ButtonEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvent : MonoBehaviour
+{
+    public bool triggered = false;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            KeyDown_A();
+            triggered = true;
+        }
+    }
+
+    public void KeyDown_A()
+    {
+        
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public string[] Patty = new string[4] { "MeatP", "Lobster", "Chickintender", "Bulgogi" };
-    public string[] Vegy = new string[5] { "Lettus", "Onion", "Relish", "Pickle", "Potato" };
-    public string[] BreadCheese = new string[3] { "BaseB", "UpperB", "Cheese" };
+    public static Spawner callObject;
+
+    public GameObject Patty_Pool;
+
+    public GameObject SpawnPoint1;
+
+    public int PattyNumb = 5;
 
 
-    public string[] Orders;
-    public int Patnum = Random.Range(0,4);
-    public int Vegnum = Random.Range(0,5);
-    Spawner() {
-            
-            Orders = new string[5]{ BreadCheese[0], Vegy[Vegnum], Patty[Patnum], BreadCheese[1], BreadCheese[2] };
+    public List<GameObject> PoolObjs_P;
+
+
+    void Start()
+    {
+        PoolObjs_P = new List<GameObject>();
+
+        for(int i = 0; i< PattyNumb; i++)
+        {
+            GameObject obj_P = (GameObject)Instantiate(Patty_Pool);
+
+            obj_P.transform.parent = SpawnPoint1.transform;///object SpawnPoint에 생성
+
+            obj_P.SetActive(false);
+            PoolObjs_P.Add(obj_P); ///만들고 비활성화해둔 오브젝트 풀에 저장.
         }
 
+    }
 
-
+    public GameObject GetPooledPatty()
+    {
+        for(int i =0; i<PoolObjs_P.Count; i++)
+        {
+            if(!PoolObjs_P[i].activeInHierarchy)
+            {
+                return PoolObjs_P[i];
+            }
+        }
+        return null; /// PoolObjs_P에 prefab이 없으면 null 넘겨줌
+    }
 }
