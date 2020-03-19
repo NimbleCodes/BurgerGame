94c4a5553e1dd2205b5c585ee6482f9e805d6506
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2020-03-19 14:46:55 +0900

    Healthbar.working

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,30 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
-    public float fDelayTime;
-    public float fCurrentTime;
-    public int iMaxX;
-    private float fVertical;
-    void Start()
-    {
-        iMaxX = (int)GetComponent<RectTransform>().rect.width;
-        fVertical = transform.Find("SlidingArea").Find("Handle").gameObject.GetComponent<RectTransform>().offsetMax.y;
-
-        fDelayTime = 10.0f;
-        fCurrentTime = 0.0f;
-    }
+    public Image cooldown;
+    public bool coolingDown;
+    public float waitTime = 30.0f;
+   /// void Start()
+    ///{
+       
+    ///}
 
     // Update is called once per frame
     void Update()
     {
-
-        fCurrentTime += Time.deltaTime;
-
-        float fT = iMaxX * (fCurrentTime / fDelayTime);
-        transform.Find("SlidingArea").Find("Handle").
-            gameObject.GetComponent<RectTransform>().offsetMax = new Vector2(fVertical + fT, 0);
+        if(coolingDown==true)
+        {
+            cooldown.fillAmount -= 1.0f / waitTime * Time.deltaTime;
+        }
+        
     }
 }
