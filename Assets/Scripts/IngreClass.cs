a701a0aa45a9b4d043d7878dc2ff6b4428ebc176
DaeHyun Kang <limitedcopy@gmail.com>
2020-03-20 15:40:50 +0900

    ingreClass와 ingrePool
    ingreClass는 객체 각자, ingrePool은 객체 전부를 관리하기 위한 코드이다.

--- Assets/Scripts/IngreClass.cs
+++ Assets/Scripts/IngreClass.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class IngreClass : MonoBehaviour
 {
+    ingredient ingrVar;
+    Rigidbody2D rigid;
+
+    private void Start() {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    //자신의 정보 초기화.
+    public void setIngrValue(ingredient value){
+        ingrVar = value;
+    }
+    
+    //rigidbody simulating을 켜거나 끈다. 그러므로 중력이 적용되지 않으며 불필요한 연산을 방지한다.
+    public void turnGravity(bool value){
+        rigid.simulated = value;
+    }
+
     
 }
---
+++ Assets/Scripts/ingrePool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ingreClass 객체들을 전체적으로 관리하기 위한 스크립트.
+public class ingrePool : MonoBehaviour
+{
+    IngreClass[] ingreArray;
+    
+    void Awake()
+    {
+            
+    }
+
+    void Update()
+    {
+        
+    }
+}
