63b1fb984e1f33b39d3da7f790adbf6e23a91b64
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2020-03-20 19:51:31 +0900

    healthmanager almost

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -6,20 +6,63 @@ using UnityEngine.UI;
 public class HealthManager : MonoBehaviour
 {
     public Image cooldown;
-    public bool coolingDown;
-    public float waitTime = 30.0f;
-    public float Score = 5.0f;
+    public bool coolingDown;//체력감소 트리거
+    public float calHealth; // 체력 계산을 위한 변수
+    public float calTime;
+    public float GameTime = 60.0f;//시작시 게임시간
+    public float minimumT = 20.0f;//최소시간
+    public float Score = 0.00003f;
+    public float minusScore = -0.00003f;
+    public float decTimeval = 5.0f;//줄어드는 시간
+
+    void Start(){
+        coolingDown = true;
+    }
 
     void Update()
     {
-        if(coolingDown==true)
-        {
-            cooldown.fillAmount -= 1.0f / waitTime * Time.deltaTime;
+        DecHealth();
+
+        if(Input.GetKeyDown(KeyCode.S)){
+            AddHealth(Score);
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            MinHealth(minusScore);
+        }
+        if(Input.GetKeyDown(KeyCode.F)){
+            DecTime();
         }
         
     }
 
-    void AddHealth(Score){
-        cooldown.fillAmount =+ Score;
+    public void AddHealth(float val){
+        cooldown.fillAmount += val;
     }
+
+    public void MinHealth(float val){
+        cooldown.fillAmount += val;
+    }
+
+    public void DecTime(){
+        if(GameTime > 0)
+        {
+            if(calTime % Time.deltaTime == 0)
+            {
+                GameTime -= decTimeval;
+            }
+        }
+        if(GameTime <= 20)//최소시간 이하로 내려가는걸 방지
+        {
+            GameTime = 20;
+        }
+    }
+
+    public void DecHealth(){
+        float percent = Time.deltaTime/GameTime;
+        calTime = Time.deltaTime;
+        if(coolingDown){
+            cooldown.fillAmount -= Mathf.Lerp(1,0,percent) * 0.0001f;
+        }
+    }
+
 }
