5d6a395a770262562a1eecb2a922570cca6cc6af
kylekim95 <kylekim95@gmail.com>
2020-03-31 13:27:49 +0900

    3/31
    난이도 조절 미완성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
     public bool incDiff = false;    //임시 코드
+    int maxNumDiff = 3;
 
     private void Start() {
         EventManager.eventManager.GamePausedEvent += OnGamePausedEvent;
+        DiffTable = new diffTable();
+        DiffTable.StageDiffVals = new diffRow[maxNumDiff];
+        LoadDiffTableFromJson();
     }
-
     private void Update()
     {
+        if (curStage < DiffTable.StageDiffVals.Length)
+            Debug.Log(DiffTable.StageDiffVals[curStage].NumActiveSpawner);
         //임시 코드
         if (incDiff)
         {
+            curStage++;
             EventManager.eventManager.Invoke_DiffIncEvent();
             incDiff = false;
         }
@@ -42,5 +49,23 @@ public class GameManager : MonoBehaviour
             prevWho = who;
             paused = true;
         }
+    }
+
+    int curStage = 0;
+    [System.Serializable]
+    struct diffRow
+    {
+        public int NumActiveSpawner;
+    }
+    [System.Serializable]
+    struct diffTable
+    {
+        public diffRow[] StageDiffVals;
+    }
+    diffTable DiffTable;
+    void LoadDiffTableFromJson()
+    {
+        string DiffTableStr = File.ReadAllText(Application.dataPath + "/Resources/Json/DiffTable.json");
+        DiffTable = JsonUtility.FromJson<diffTable>(DiffTableStr);
     }
 }
