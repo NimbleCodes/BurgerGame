099f9ab798680df638b91ebd6ed391ae5bf69278
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2020-04-05 23:07:47 +0900

    0405

--- Assets/Scripts/Spawn/showEaten.cs
+++ Assets/Scripts/Spawn/showEaten.cs
@@ -6,17 +6,20 @@ public class showEaten : MonoBehaviour
 {
     public string ingre = "Lettuce 1";//for test
     //이벤트 매니저에 함수추가
-   public void start(){
+   public void Start(){
        EventManager.eventManager.IngrEatenEvent += showEatenToUser;
    }
-   public void update(){//for test
+   public void Update(){//for test
        if(Input.GetKeyDown(KeyCode.H)){
             showEatenToUser(ingre);
         }
    }
+   //먹은 ingre_name을 받아 맞는 sprite를 호출
    public void showEatenToUser(string ingre_info){
         GameObject objToSpawn = ObjectManager.objectManager.getGameObject(ingre_info);
         objToSpawn.GetComponent<Transform>().position = new Vector3(8f,5f);
         objToSpawn.SetActive(true);
+        //objToSpawn.GetComponent<Rigidbody2D>().AddForce(Vector2.down*100f);
+        objToSpawn.GetComponent<Rigidbody2D>().gravityScale=1f;
    }
 }
