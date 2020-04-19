2f3bfe7a00a6181f8f35ff227fe626c0a3316dbc
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2020-04-19 19:38:47 +0900

    showEaten fixed(as auto)

--- Assets/Scripts/Spawn/showEaten.cs
+++ Assets/Scripts/Spawn/showEaten.cs
@@ -7,9 +7,8 @@ public class showEaten : MonoBehaviour
     public string ingre = "Lettuce";//for test
     public int arrIndex = 0;
     public static showEaten ShowObtain;
-    public GameObject ingr;//오브젝트 저장
+    public GameObject[] ingr;//오브젝트 저장
     public SpriteRenderer spriteR;
-    public int TagCount = 0;
     public string Bun = "Bun";
     public bool complete = true;
     public void Awake()
@@ -20,6 +19,7 @@ public class showEaten : MonoBehaviour
     public void Start()
     {
         //EventManager.eventManager.IngrObtainedEvent += showEatenToUser;
+        ingr = new GameObject[10];
         InitiateObj();
     }
     public void Update()
@@ -35,22 +35,22 @@ public class showEaten : MonoBehaviour
         arrIndex = 1;
         for (int i = 0; i < 10; i++)
         {
-            ingr = GameObject.FindGameObjectWithTag("showEat" + i);
-            spriteR = ingr.GetComponent<SpriteRenderer>();
-            ingr.GetComponent<Rigidbody2D>().gravityScale = 0f;
-            ingr.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-            ingr.GetComponent<Transform>().position = new Vector3(8f, 5f);
-            ingr.GetComponent<BoxCollider2D>().enabled = false;
+            ingr[i] = GameObject.FindGameObjectWithTag("showEat" + i);
+            spriteR = ingr[i].GetComponent<SpriteRenderer>();
+            ingr[i].GetComponent<Rigidbody2D>().gravityScale = 0f;
+            ingr[i].GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            ingr[i].GetComponent<Transform>().position = new Vector3(8f, 5f);
+            ingr[i].GetComponent<BoxCollider2D>().enabled = false;
             spriteR.enabled = false;
         }
-        ingr = GameObject.FindGameObjectWithTag("showEat0");
-        spriteR = ingr.GetComponent<SpriteRenderer>();
+        ingr[0] = GameObject.FindGameObjectWithTag("showEat0");
+        spriteR = ingr[0].GetComponent<SpriteRenderer>();
         spriteR.sprite = Resources.Load<Sprite>("Sprites/Ingredients/BelowBun");
         spriteR.enabled = true;
-        ingr.GetComponent<BoxCollider2D>().size = new Vector3(2f, 0.2f);
-        ingr.GetComponent<Transform>().position = new Vector3(8f, 2.2f);
-        ingr.GetComponent<Rigidbody2D>().gravityScale = 1f;
-        ingr.GetComponent<BoxCollider2D>().enabled = true;
+        ingr[0].GetComponent<BoxCollider2D>().size = new Vector3(2f, 0.2f);
+        ingr[0].GetComponent<Transform>().position = new Vector3(8f, 2.2f);
+        ingr[0].GetComponent<Rigidbody2D>().gravityScale = 1f;
+        ingr[0].GetComponent<BoxCollider2D>().enabled = true;
     }
     //먹은 ingre_name을 받아 맞는 sprite를 호출
     public void showEatenToUser(string ingre_info)
@@ -64,14 +64,13 @@ public class showEaten : MonoBehaviour
         }
         else
         {
-            
-            ingr = GameObject.FindGameObjectWithTag("showEat" + arrIndex);
-            spriteR = ingr.GetComponent<SpriteRenderer>();
+            //ingr[arrIndex] = GameObject.FindGameObjectWithTag("showEat" + arrIndex);
+            spriteR = ingr[arrIndex].GetComponent<SpriteRenderer>();
             spriteR.sprite = Resources.Load<Sprite>("Sprites/Ingredients/" + ingre_info);
             spriteR.enabled = true;
-            ingr.GetComponent<BoxCollider2D>().size = new Vector3(2f, 0.2f);
-            ingr.GetComponent<Rigidbody2D>().gravityScale = 1f;
-            ingr.GetComponent<BoxCollider2D>().enabled = true;
+            ingr[arrIndex].GetComponent<BoxCollider2D>().size = new Vector3(2f, 0.2f);
+            ingr[arrIndex].GetComponent<Rigidbody2D>().gravityScale = 1f;
+            ingr[arrIndex].GetComponent<BoxCollider2D>().enabled = true;
             arrIndex++;
 
         }
