2aa63522355e28c4429a622c9af42262192c6199
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2020-05-27 17:05:28 +0900

    tutorial UI added
    UI 추가 및 필요 함수들 코드 추가

--- Assets/Scripts/Tutorial/BpWatcher.cs
+++ Assets/Scripts/Tutorial/BpWatcher.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
 
 class BpWatcher : MonoBehaviour
 {
+    public Image ingr_highlight;
+    public Image lane_highlight;
+    public Image trigpoint_highlight;
+    public Image trigger_highlight;
+    public TextMeshProUGUI ingr_text;
+    public TextMeshProUGUI lane_text;
+    public TextMeshProUGUI trigpoint_text;
+    public TextMeshProUGUI trigger_text;
+    Action startQue;
     Queue<Breakpoint> breakpoints;
     bool timerStart = false;
 
@@ -16,6 +28,7 @@ class BpWatcher : MonoBehaviour
             breakpoints.Peek().active = true;
     }
 
+
     private void Awake()
     {
         breakpoints = new Queue<Breakpoint>();
@@ -26,6 +39,7 @@ class BpWatcher : MonoBehaviour
         if (breakpoints.Count > 0)
             breakpoints.Peek().active = true;
     }
+    
     private void Update()
     {
         if (breakpoints.Count > 0)
@@ -54,5 +68,33 @@ class BpWatcher : MonoBehaviour
         {
             gameObject.GetComponent<BpWatcher>().enabled = false;
         }
+    }
+
+
+//-----------Order[ingr -> lane -> trigpoint -> trigger]
+    private void Active_ingr(){ //Activate ingr tutorial
+        ingr_highlight.enabled = true;
+        ingr_text.enabled = true;
+    }
+
+    private void Active_lane(){//Activate lane tutorial
+        ingr_highlight.enabled =false;
+        ingr_text.enabled = false;
+        lane_highlight.enabled = true;
+        lane_text.enabled = true;
+    }
+
+    private void Active_trigpoint(){//Active trigpoint tutorial
+        lane_highlight.enabled = false;
+        lane_text.enabled = false;
+        trigpoint_highlight.enabled = true;
+        trigpoint_text.enabled = true;
+    }
+
+    private void Active_trigger(){//Active trigger tutorial
+        trigpoint_highlight.enabled = false;
+        trigpoint_text.enabled = false;
+        trigger_highlight.enabled = true;
+        trigger_text.enabled = true;
     }
 }
